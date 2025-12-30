9066ea70f365128f5a1fbf0a7207e359836ad3bc
Sigmund Ørjavik <72895976+lurulf@users.noreply.github.com>
2025-12-30 19:35:59 +0000

    ny algoritme

--- bash/aoc7_2.sh
+++ bash/aoc7_2.sh
@@ -5,31 +5,40 @@ set -eufo pipefail
 mapfile -t manifold <input_aoc7.txt
 startx=0
 linje="${manifold[0]}"
+declare -a straaler
+declare -a forrigestraaler
+declare -a tomstraaler
 for ((t = 0; t < ${#linje}; t++)); do
   if [[ ${linje:t:1} == "S" ]]; then
     startx="${t}"
   fi
+  straaler[t]=0
+  tomstraaler[t]=0
 done
 
-straale() {
-  local x y
-  if (($# < 2)); then
-    echo "Straale kalt med for få parametre: $#"
-    exit 1
-  fi
-  x="$1"
-  y="$2"
-  while ((y < ${#manifold})); do
-    if [[ ${manifold[y]:x:1} == "^" ]]; then
-      echo "$(($(straale "$((x - 1))" "${y}") + $(straale "$((x + 1))" "${y}")))"
-      break
-    else
-      y=$((y + 1))
-    fi
-  done
-  if ((y == ${#manifold})); then
-    echo "1"
-  fi
-}
+straaler[startx]=1
 
-straale "${startx}" 0
+for ((l = 1; l < ${#manifold}; l++)); do
+  linje="${manifold[l]}"
+  forrigestraaler=("${straaler[@]}")
+  straaler=("${tomstraaler[@]}")
+  for ((t = 0; t < ${#linje}; t++)); do
+    case "${linje:t:1}" in
+      ".")
+        straaler[t]="$((straaler[t] + forrigestraaler[t]))"
+        ;;
+      "^")
+        straaler[t - 1]=$((straaler[t - 1] + forrigestraaler[t]))
+        straaler[t + 1]=$((straaler[t + 1] + forrigestraaler[t]))
+        ;;
+      *) ;;
+    esac
+  done
+done
+
+sum=0
+for ((t = 0; t < ${#linje}; t++)); do
+  sum=$((sum + straaler[t]))
+done
+
+echo "${sum}"
---
+++ bash/bak/aoc7_2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# shellcheck disable=SC2312
+set -eufo pipefail
+
+# Rekursiv algoritme. Den gir riktig svar, men vil aldri fullføre inne rimelig tid
+
+mapfile -t manifold <input_aoc7.txt
+startx=0
+linje="${manifold[0]}"
+for ((t = 0; t < ${#linje}; t++)); do
+  if [[ ${linje:t:1} == "S" ]]; then
+    startx="${t}"
+  fi
+done
+
+straale() {
+  local x y
+  if (($# < 2)); then
+    echo "Straale kalt med for få parametre: $#"
+    exit 1
+  fi
+  x="$1"
+  y="$2"
+  while ((y < ${#manifold})); do
+    if [[ ${manifold[y]:x:1} == "^" ]]; then
+      echo "$(($(straale "$((x - 1))" "${y}") + $(straale "$((x + 1))" "${y}")))"
+      break
+    else
+      y=$((y + 1))
+    fi
+  done
+  if ((y == ${#manifold})); then
+    echo "1"
+  fi
+}
+
+straale "${startx}" 0
