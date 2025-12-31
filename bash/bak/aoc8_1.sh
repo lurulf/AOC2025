69cad0e44294fe1fcdf31c730e55d9c9159ff9a0
Sigmund Ørjavik <72895976+lurulf@users.noreply.github.com>
2025-12-31 08:42:40 +0000

    AOC8_1

--- bash/bak/aoc8_1.sh
+++ bash/bak/aoc8_1.sh
@@ -14,32 +14,58 @@ for ((t = 0; t < ${#punkter[@]}; t++)); do
   kretsmedlemmer[t]=""
 done
 
+# echo "${#punkter[@]}"
+
+# printf "     "
+# for ((t = 0; t < ${#kretsmedlem[@]}; t++)); do
+# printf "%2s " "${t}"
+# done
+# echo ""
+
 teller=0
 while IFS= read -r avstand; do
+  # echo "${avstand}"
   IFS=, read -r -a data <<<"${avstand}"
+  # echo "${data[*]}"
   kd1="${kretsmedlem[data[1]]}"
   kd2="${kretsmedlem[data[2]]}"
+  # printf "%4s %2s %2s %s\n" "${teller}" "${kd1}" "${kd2}" "${data[*]}"
   if ((kd1 < 0 && kd2 < 0)); then
     makskretser=$((makskretser + 1))
+    # echo "ny krets: ${makskretser} = ${data[1]} og ${data[2]}"
     kretsmedlem[data[1]]="${makskretser}"
     kretsmedlem[data[2]]="${makskretser}"
     kretsmedlemmer[makskretser]="${data[1]},${data[2]}"
   elif ((kd1 < 0)); then
+    # echo "kd1: ${data[1]} til krets ${kd2}"
     kretsmedlem[data[1]]="${kd2}"
     kretsmedlemmer[kd2]+=",${data[1]}"
   elif ((kretsmedlem[data[2]] < 0)); then
+    # echo "kd2: ${data[2]} til krets ${kd1}"
     kretsmedlem[data[2]]="${kd1}"
     kretsmedlemmer[kd1]+=",${data[2]}"
   else
     if ((kd1 != kd2)); then
+      # echo "koble sammen kretsene ${kd1} og ${kd2}"
       IFS=, read -r -a medlemmer <<<"${kretsmedlemmer[kd2]}"
+      # echo "${kretsmedlemmer[kd2]}"
+      # echo "${medlemmer[*]}"
       for ((t = 0; t < ${#medlemmer[@]}; t++)); do
+        # echo "${t} ${medlemmer[t]}"
         kretsmedlem[medlemmer[t]]="${kd1}"
       done
       kretsmedlemmer[kd1]+=",${kretsmedlemmer[kd2]}"
       kretsmedlemmer[kd2]=""
+    else
+      # echo "${data[1]} og ${data[2]} er allerede i samme krets: ${kd1}"
+      true
     fi
   fi
+  # printf "%4s " "${teller}"
+  # printf "%2s " "${kretsmedlem[@]}"
+  # printf "%2s %2s %2s %2s %10s" "${kd1}" "${kd2}" "${data[1]}" "${data[2]}" "${data[0]}"
+  # echo ""
+  # echo "${kretsmedlemmer[*]}"
   teller=$((teller + 1))
 done < <(
   {
@@ -55,6 +81,14 @@ done < <(
     done
   } | sort -t, -k1,1n | sed -n "1,${antall}p"
 )
+
+# echo "===================================="
+# printf "%s\n" "${kretsmedlem[@]}"
+echo "===================================="
+printf "%s\n" "${kretsmedlem[@]}" | sort | uniq -c | sort -rn
+echo "===================================="
+printf "%s\n" "${kretsmedlem[@]}" | sort | uniq -c | sort -rn | grep -v -- '-1' | sed -n "1,3p"
+echo "===================================="
 
 faktor=1
 while read -r -a data; do
